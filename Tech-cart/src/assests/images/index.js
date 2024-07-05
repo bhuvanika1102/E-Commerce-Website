95e10ef8876ce1ac73e26416a5cada0aebead0b9
BHUVANIKA <149864996+bhuvanika1102@users.noreply.github.com>
2024-07-05 23:28:54 +0530

    Add files via upload

---
+++ Tech-cart/src/assests/images/index.js
@@ -0,0 +1,182 @@
+import logo from "./logoLight.jpg";
+import logoLight from "./logoLight.jpg";
+import bannerImgOne from "./banner/bannerImgOne.webp";
+import bannerImgTwo from "./banner/bannerImgTwo.webp";
+import bannerImgThree from "./banner/bannerImgThree.webp";
+import saleImgOne from "./sale/saleImgOne.jpg";
+import saleImgTwo from "./sale/saleImgTwo.jpeg";
+import saleImgThree from "./sale/bracelettttt.webp";
+// ============== Products Start here ====================
+import pdf1 from "../pdf/pdf1.pdf";
+// New Arrivals
+import watch from "./products/newArrival/watch.jpeg";
+import gift3 from "./products/newArrival/gift3.webp";
+import gift1 from "./products/newArrival/gift1.webp";
+import gift2 from "./products/newArrival/gift2.webp";
+
+// Best Sellers
+import gift22 from "./products/bestSeller/gift22.jpeg";
+import gift33 from "./products/bestSeller/gift33.webp";
+import naruto from "./products/bestSeller/naruto.jpeg";
+import unicorn from "./products/bestSeller/unicorn.jpeg";
+import imprimante1 from "./products/bestSeller/imprimante1.webp";
+import imprimante2 from "./products/bestSeller/imprimante2.webp";
+import imprimante4 from "./products/bestSeller/imprimante4.webp";
+import imprimante5 from "./products/bestSeller/imprimante5.webp";
+import imprimante6 from "./products/bestSeller/imprimante6.webp";
+import imprimante7 from "./products/bestSeller/imprimante7.webp";
+import imprimante8 from "./products/bestSeller/imprimante8.webp";
+import imprimante9 from "./products/bestSeller/imprimante9.webp";
+import cup1 from "./products/bestSeller/cup1.jpg";
+import cup2 from "./products/bestSeller/cup2.jpg";
+import cup3 from "./products/bestSeller/cup3.jpg";
+import cup4 from "./products/bestSeller/cup4.jpg";
+import bac1 from "./products/bestSeller/bac1.webp";
+import bac2 from "./products/bestSeller/bac2.webp";
+import bac3 from "./products/bestSeller/bac3.webp";
+import bac4 from "./products/bestSeller/bac4.webp";
+import clock1 from "./products/bestSeller/clock1.jpeg";
+import clock2 from "./products/bestSeller/clock2.jpeg";
+import clock4 from "./products/bestSeller/clock4.jpeg";
+import clock5 from "./products/bestSeller/clock5.jpeg";
+import clock6 from "./products/bestSeller/clock6.jpeg";
+import clock7 from "./products/bestSeller/clock7.jpeg";
+import clock8 from "./products/bestSeller/clock8.jpeg";
+import clock9 from "./products/bestSeller/clock9.jpeg";
+import clock10 from "./products/bestSeller/clock10.jpeg";
+import bracelet1 from "./products/bestSeller/bracelet1.jpeg";
+import bracelet2 from "./products/bestSeller/bracelet2.jpeg";
+import bracelet3 from "./products/bestSeller/bracelet3.jpeg";
+import bracelet4 from "./products/bestSeller/bracelet4.jpeg";
+import bracelet5 from "./products/bestSeller/bracelet5.jpeg";
+import bracelet6 from "./products/bestSeller/bracelet6.jpeg";
+
+
+// Special Offers
+import spfOne from "./products/specialOffer/spfOne.webp";
+import spfTwo from "./products/specialOffer/spfTwo.webp";
+import spfThree from "./products/specialOffer/spfThree.webp";
+import spfFour from "./products/specialOffer/spfFour.webp";
+
+// Year Product
+import productOfTheYear from "./products/productOfTheYear.webp";
+// ============== Products End here ======================
+import paymentCard from "./payment.png";
+import emptyCart from "../images/emptyCart.png";
+
+import teddy from "../images/products/imprimante/teddy.jpg";
+import tedddy6 from "../images/products/imprimante/tedddy6.jpg";
+import teddy4 from "../images/products/imprimante/teddy4.jpg";
+import teddy2 from "../images/products/imprimante/teddy2.jpg";
+import teddy7 from "../images/products/imprimante/teddy7.jpg";
+import teddy8 from "../images/products/imprimante/teddy8.jpg";
+import teddy9 from "../images/products/imprimante/teddy9.jpg";
+import tedddy14 from "../images/products/imprimante/tedddy14.jpg";
+import tedddy15 from "../images/products/imprimante/tedddy15.jpg";
+
+import tedddy17 from "../images/products/imprimante/tedddy17.jpg";
+import tedddy117 from "../images/products/imprimante/tedddy117.jpg";
+import tedddy1118 from "../images/products/imprimante/tedddy1118.jpg";
+
+import tedddy16 from "../images/products/imprimante/tedddy16.jpg";
+import b from "../images/products/imprimante/b.webp";
+import tedddy119 from "../images/products/imprimante/tedddy119.jpg";
+import now from "../images/products/imprimante/now.webp";
+import a from "../images/products/imprimante/a.webp";
+
+import c from "../images/products/imprimante/c.webp";
+import tedddyy from "../images/products/imprimante/tedddyy.jpg";
+import BestSellers from './../../components/home/BestSellers/BestSellers';
+
+export {
+  logo,
+  logoLight,
+  bannerImgOne,
+  bannerImgTwo,
+  bannerImgThree,
+  saleImgOne,
+  saleImgTwo,
+  saleImgThree,
+  // ===================== Products start here ============
+  // New Arrivals
+  watch,
+  gift3,
+  gift1,
+  gift2,
+
+  // Best Sellers
+  naruto,
+  gift33,
+  gift22,
+  unicorn,
+  imprimante1,
+  imprimante2,
+  imprimante4,
+  imprimante5,
+  imprimante6,
+  imprimante7,
+  imprimante8,
+  imprimante9,
+  cup1,
+  cup2,
+  cup3,
+  cup4,
+  bac1,
+  bac2,
+  bac3,
+  bac4,
+  clock1,
+  clock2,
+  clock4,
+  clock5,
+  clock6,
+  clock7,
+  clock8,
+  clock9,
+  clock10,
+  bracelet1,
+  bracelet2,
+  bracelet3,
+  bracelet4,
+  bracelet5,
+  bracelet6,
+  
+
+  // Sprcial Offers
+  spfOne,
+  spfTwo,
+  spfThree,
+  spfFour,
+
+  // Year Product
+  productOfTheYear,
+  // ===================== Products End here ==============
+  paymentCard,
+  emptyCart,
+  pdf1,
+
+  // imprimante pantum
+  teddy,
+  tedddy6,
+  teddy4,
+  teddy2,
+  teddy7,
+  teddy8,
+  teddy9,
+  tedddy14,
+  tedddy15,
+
+  // imprimante Hp
+  tedddy17,
+  tedddy117,
+  tedddy1118,
+
+  // imprimante Ricoh
+  tedddy16,
+  b,
+  tedddy119,
+  now,
+  a,
+  c,
+  tedddyy,
+};
